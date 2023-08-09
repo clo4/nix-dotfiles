0abd3dd27020e306e1d6200936e1c4d5cbd05e87
clo4 <git@clo4.net>
2023-08-09 18:01:23 +1000

    vm: fix

--- home/utm/default.nix
+++ home/utm/default.nix
@@ -1,17 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ../common.nix
   ];
 
   home.homeDirectory = "/home/robert";
 
-  users.users.robert = {
-    extraGroups = ["wheel" "networkmanager"];
-    hashedPassword = "$y$j9T$/DELHBb5Gc.uI/Cyr6KGo1$AgxXRZnEcH74IJnaN.L4VOXLllAMeNrX4IhJCsNYu86";
-  };
+  home.stateVersion = "23.05";
 
-  users.mutableUsers = false;
-
-  networking.hostName = "robert-nixos-utm";
-  system.stateVersion = "23.05";
+  home.packages = with pkgs; [
+    libsForQt5.dolphin
+  ];
 }

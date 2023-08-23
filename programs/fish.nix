69fa60ec85c6a44f6fbfc6312d6e63553203c6a7
clo4 <git@clo4.net>
2023-08-23 14:09:46 +1000

    fish: fix abbreviation expansion bindings

--- programs/fish.nix
+++ programs/fish.nix
@@ -37,6 +37,17 @@ in {
             fish_default_key_bindings
             bind \cz 'fg 2>/dev/null; commandline -f repaint'
             bind \ez 'zi; commandline -f repaint'
+
+            # Not sure why but the order of these is broken by default.
+            # expand-abbr needs to happen first so the cursor is
+            # still over the abbreviation when it tries to expand.
+            bind ' ' expand-abbr self-insert
+            bind ';' expand-abbr self-insert
+            bind '|' expand-abbr self-insert
+            bind '&' expand-abbr self-insert
+            bind '>' expand-abbr self-insert
+            bind '<' expand-abbr self-insert
+            bind ')' expand-abbr self-insert
           '';
 
           # Displays every path in $PATH on new lines.

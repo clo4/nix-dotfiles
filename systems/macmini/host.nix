c79434576ddc85be42f8239ef50c363f6476ed7b
clo4 <git@clo4.net>
2023-09-15 22:04:14 +1000

    macmini: configure `defaults` via nix too

--- systems/macmini/host.nix
+++ systems/macmini/host.nix
@@ -20,11 +20,34 @@
 
   networking.hostName = "macmini";
 
-  # Hides desktop icons (but they're still accessible through Finder)
-  system.defaults.finder.CreateDesktop = false;
-
   # TODO: Should this be moved to the common config?
   services.nix-daemon.enable = true;
 
   system.stateVersion = 4;
+
+  system.defaults.NSGlobalDomain = {
+    AppleInterfaceStyleSwitchesAutomatically = true;
+    ApplePressAndHoldEnabled = false;
+    AppleShowAllExtensions = true;
+    NSAutomaticCapitalizationEnabled = false;
+    NSAutomaticPeriodSubstitutionEnabled = false;
+    NSAutomaticSpellingCorrectionEnabled = false;
+    InitialKeyRepeat = 15;
+    KeyRepeat = 2;
+    "com.apple.keyboard.fnState" = true;
+  };
+
+  system.defaults.dock.autohide = true;
+
+  system.defaults.LaunchServices.LSQuarantine = false;
+
+  system.defaults.finder = {
+    ShowPathbar = true;
+    # Hides desktop icons (but they're still accessible through Finder)
+    CreateDesktop = false;
+    # This magic string makes it search the current folder by default
+    FXDefaultSearchScope = "SCcf";
+    # Use the column view by default-- the obviously correct and best view
+    FXPreferredViewStyle = "clmv";
+  };
 }

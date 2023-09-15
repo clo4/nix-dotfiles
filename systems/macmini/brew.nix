0cee90803c520b13e3936a5787ef0d322ceeec6c
clo4 <git@clo4.net>
2023-09-15 23:50:39 +1000

    macmini: add more brew casks

--- systems/macmini/brew.nix
+++ systems/macmini/brew.nix
@@ -2,17 +2,36 @@
   environment.systemPath = ["/opt/homebrew/bin"];
   homebrew.enable = true;
 
+  # Applications are installed through Homebrew because there's a wider selection available
+  # on macOS and the applications tend to be more up-to-date.
   homebrew.casks = [
+    # homerow isn't available as a cask yet
     "1password"
+    "affinity-designer"
+    "affinity-photo"
+    "affinity-publisher"
+    "appcleaner"
+    "arc"
+    "audio-hijack"
+    "bartender"
     "elgato-camera-hub"
     "grandperspective"
     "hammerspoon"
     "iina"
+    "karabiner-elements"
+    "linearmouse"
+    "little-snitch"
+    "loopback"
+    "monitorcontrol"
     "obs"
     "obsidian"
+    "plover"
     "raycast"
     "rectangle"
     "rio"
+    "spotify"
+    "wezterm"
+    "zed"
     "zed"
   ];
 }

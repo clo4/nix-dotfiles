a64b4c97eb488db1b541c6b07a33cb52a3c60daa
clo4 <git@clo4.net>
2023-12-15 14:54:52 +1100

    brew: update casks

--- shared/brew.nix
+++ shared/brew.nix
@@ -29,6 +29,9 @@
 
   # Applications are installed through Homebrew because there's a wider selection available
   # on macOS and the applications tend to be more up-to-date.
+  # The other reason is that applications installed via Nix tend to break in the dock because
+  # of the way the volume mounts - it's been a while but from what I remember the icons weren't
+  # working correctly, or maybe it's that opening them on startup was having problems?
   homebrew.casks = [
     # NOTE: Homerow isn't available as a cask yet
 
@@ -45,6 +48,9 @@
     # Loosely, productivity
     "raycast"
     "appcleaner"
+    "shottr"
+    "keka"
+    "karabiner-elements" # Might make a module for this in the future...
 
     # Loosely, social platforms
     "signal"

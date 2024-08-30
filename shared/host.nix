f7dec35c5acd96b2f9725aa81c03ac7dc6acb87b
clo4 <git@clo4.net>
2024-08-31 09:09:38 +1000

    nix: set nixpkgs in registry to nixpkgs-unstable

--- shared/host.nix
+++ shared/host.nix
@@ -40,8 +40,8 @@
   };
 
   nix.registry = {
-    # Makes `nix run nixpkgs#...` run using the nixpkgs from this flake
-    nixpkgs.flake = inputs.nixpkgs;
+    # Makes `nix run nixpkgs#...` run using the latest unstable
+    nixpkgs.flake = inputs.nixpkgs-unstable;
 
     # inputs.self is a reference to this flake, which allows self-references.
     # In this case, adding this flake to the registry under the name `my`,

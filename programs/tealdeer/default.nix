a7e1759946fc2b9046fe27d6b527249033d49f4b
clo4 <git@clo4.net>
2024-12-28 13:18:47 +1100

    Clean up tealdeer configuration

--- programs/tealdeer/default.nix
+++ programs/tealdeer/default.nix
@@ -15,22 +15,7 @@ in
     enable = mkEnableOption "my tealdeer configuration";
   };
   config = mkIf cfg.enable {
-    # Using a custom patch to override the default maximum cache age, because
-    # the tldr-pages cache is managed by Nix which sets the last modified date
-    # to 1970.
-    # For some reason I couldn't get this to work right with an overlay, so
-    # this may cause problems with conflicts in the future... we'll see.
-    home.packages = [
-      # (pkgs.tealdeer.overrideAttrs (o: {
-      #   pname = "tealdeer-patched";
-      #   patches =
-      #     (o.patches or [])
-      #     ++ [
-      #       ./no-max-cache-age.patch
-      #     ];
-      # }))
-      pkgs.tealdeer
-    ];
+    home.packages = [ pkgs.tealdeer ];
     home.file."${cachesDir}/tealdeer/tldr-pages".source = inputs.tldr-pages;
   };
 }

de41f51d57f90f4aac258206e01432770096850f
clo4 <robert@clo4.net>
2025-02-03 07:12:42 +1100

    Fully remove nix-env fish plugin

--- hosts/macbook-air/users/robert.nix
+++ hosts/macbook-air/users/robert.nix
@@ -14,19 +14,6 @@
 
   my.config.directory = "Developer/nix-dotfiles/config";
 
-  # TODO: Since ZSH is the login shell, and it hasn't sourced hm-session-vars,
-  # the PATH isn't set up correctly and it can't point to Fish.
-  # Additionally, fish is in a different path, so I need a different way to
-  # execute it than hardcoding the path in my Ghostty config.
-  my.programs.fish.plugins = [
-    # (pkgs.fetchFromGitHub {
-    #   owner = "lilyball";
-    #   repo = "nix-env.fish";
-    #   rev = "7b65bd228429e852c8fdfa07601159130a818cfa"; # as of 2025-01-14
-    #   hash = "sha256-RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
-    # })
-  ];
-
   # FIXME: This may not be working. Figure out why. Does it require logout?
   targets.darwin.currentHostDefaults = {
     NSGlobalDomain = {

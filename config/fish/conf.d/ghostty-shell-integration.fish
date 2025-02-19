0183e2d2673fabaec566d95a7c431e60859514cf
clo4 <robert@clo4.net>
2025-02-20 10:26:04 +1100

    Move ghostty init to its own file
    It was already partially in there, but not fully. Don't know why I did
    that initially, but now it's easy to link to as an example of how to
    manually set up the integration.

--- config/fish/conf.d/ghostty-shell-integration.fish
+++ config/fish/conf.d/ghostty-shell-integration.fish
@@ -1,4 +1,5 @@
 if set -q GHOSTTY_RESOURCES_DIR GHOSTTY_BIN_DIR
     source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
     set --append fish_complete_path "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_completions.d"
+    set --append PATH $GHOSTTY_BIN_DIR
 end

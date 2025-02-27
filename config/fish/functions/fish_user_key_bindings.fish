8120357b412dbe07d3fec3b3587b291d4a728ad7
clo4 <robert@clo4.net>
2025-02-28 00:53:30 +1100

    Add C-g binding: select files with changes

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -3,6 +3,9 @@ function fish_user_key_bindings
     bind \cz 'fg 2>/dev/null; commandline -f repaint'
     bind \ez 'zi; commandline -f repaint'
 
+    # c-g for git status
+    bind \cg _fzf_git_status_modified
+
     # Not sure why but the order of these is broken by default.
     # expand-abbr needs to happen first so the cursor is
     # still over the abbreviation when it tries to expand.
@@ -13,4 +16,10 @@ function fish_user_key_bindings
     bind '>' expand-abbr self-insert
     bind '<' expand-abbr self-insert
     bind ')' expand-abbr self-insert
+end
+
+function _fzf_git_status_modified
+    set -l selected (git status --porcelain | sed 's/^.. //' | fzf --query (commandline --current-token) --multi --layout=reverse --height=40% --cycle)
+    commandline --current-token --replace -- (string join ' ' -- $selected)
+    commandline -f repaint
 end

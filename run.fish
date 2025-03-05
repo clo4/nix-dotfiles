0a3bd9eb90c471f008e48d9a5dc1da15be59d7a9
clo4 <robert@clo4.net>
2025-03-05 11:42:37 +1100

    Use hostnames for function names, add shorter aliases
    Using fish for my command runner has started to pay off already :)

--- run.fish
+++ run.fish
@@ -15,7 +15,7 @@ function _validate_system_verb
     end
 end
 
-function server -d "Build and switch homeserver1 NixOS configuration"
+function homeserver1 -d "Build and switch homeserver1 NixOS configuration"
     _validate_system_verb $argv
 
     nixos-rebuild $argv[1] \
@@ -26,18 +26,21 @@ function server -d "Build and switch homeserver1 NixOS configuration"
         --use-remote-sudo \
         $argv[2..]
 end
+alias hs homeserver1
 
 function macmini -d "Build and switch macmini nix-darwin configuration"
     _validate_system_verb $argv
 
     darwin-rebuild $argv[1] --flake .#macmini --max-jobs 8 $argv[2..]
 end
+alias mm macmini
 
-function macbook -d "Build and switch robert@macbook-air Home Manager configuration"
+function macbook-air -d "Build and switch robert@macbook-air Home Manager configuration"
     _validate_system_verb $argv
 
     home-manager $argv[1] --flake .#robert@macbook-air --max-jobs 8 $argv[2..]
 end
+alias mb macbook-air
 
 function rcon -d "Connect to homeserver1 and begin an interactive RCON session"
     echo (set_color --italics)"connecting to homeserver1 and executing rcon-cli..."(set_color normal)

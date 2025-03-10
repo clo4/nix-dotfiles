ad9510f14d6f51d0b018088df73322fa51478d3a
clo4 <robert@clo4.net>
2025-03-10 19:00:20 +1100

    Set IN_NIX_SHELL in 'nix shell'

--- config/fish/functions/nix.fish
+++ config/fish/functions/nix.fish
@@ -8,10 +8,16 @@ function nix --wraps nix
             case develop
                 announce nix develop --command (status fish-path) $argv[2..]
             case shell
-                announce nix shell $argv[2..] --command (status fish-path)
+                # eelco has stated that IN_NIX_SHELL will not be added to 'nix shell'
+                # because its behavior differs from 'nix-shell' and 'nix develop', but
+                # from a user's perspective, I'm still entering a subshell launched by
+                # Nix, so I want to know about that.
+                # Nix sets IN_NIX_SHELL to either 'pure' or 'impure'. I'm using an
+                # alternative that differentiates it while still setting it.
+                IN_NIX_SHELL=shell announce nix shell $argv[2..] --command (status fish-path)
             case '*'
                 command nix $argv
-            end
+        end
     else
         command nix $argv
     end

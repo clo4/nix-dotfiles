c4b57e9a1f3ff86d55236a940b26b7ba5b705359
clo4 <robert@clo4.net>
2025-03-11 15:44:24 +1100

    run: assume user is using devshell

--- run.fish
+++ run.fish
@@ -1,8 +1,13 @@
 # @fish-lsp-disable 2002 4004
 
+set -g run_depth 0
+
 function _run
+    set -g run_depth (math "$run_depth + 1")
+    set stem (string repeat --count $run_depth "~~")
+
     set colored_command (string escape -- $argv | string join ' ' | fish_indent --ansi)
-    echo "$(set_color brgreen --bold)running: $(set_color normal)$colored_command"
+    echo "$(set_color brgreen --bold)$stem>$(set_color normal) $colored_command"
     $argv
 end
 
@@ -13,19 +18,19 @@ function _homeserver1 -a verb
     if test (hostname -s) != homeserver1
         set --prepend rebuild_args --target-host robert@homeserver1 --build-host robert@homeserver1
     end
-    _run nix run --inputs-from . nixpkgs#nixos-rebuild -- $verb $rebuild_args
+    _run $verb $rebuild_args
 end
 
 alias macmini-build "_macmini build"
 alias macmini-switch "_macmini switch"
 function _macmini -a verb
-    _run nix run --inputs-from . nix-darwin#default -- $verb --flake .#macmini --max-jobs 8 $argv[2..]
+    _run darwin-rebuild $verb --flake .#macmini --max-jobs 8 $argv[2..]
 end
 
 alias macbook-air-build "_macbook-air build"
 alias macbook-air-switch "_macbook-air switch"
 function _macbook-air -a verb
-    _run nix run --inputs-from . home-manager#default -- $verb --flake .#robert@macbook-air --max-jobs 8 $argv[2..]
+    _run home-manager $verb --flake .#robert@macbook-air --max-jobs 8 $argv[2..]
 end
 
 function rcon -d "Connect to homeserver1 and begin an interactive RCON session"

b094801d30d54566fb269688431e13ce070df25c
clo4 <robert@clo4.net>
2025-03-11 12:22:29 +1100

    Move __source_init to conf.d/init.fish

--- config/fish/conf.d/init.fish
+++ config/fish/conf.d/init.fish
@@ -1,4 +1,12 @@
-# __source_init will only run the command if the first argument exists as a function or command
-__source_init zoxide init fish
-__source_init direnv hook fish
-__source_init fzf --fish
+function __source
+    # Using type instead of command allows for functions too
+    if type -q $argv[1]
+        $argv | source
+    end
+end
+
+__source zoxide init fish
+__source direnv hook fish
+__source fzf --fish
+
+functions -e __source

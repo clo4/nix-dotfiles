cc533c559b96f238d04715c3aa4c37f4cf6699a6
clo4 <robert@clo4.net>
2025-03-14 00:02:42 +1100

    run: hide errors printed to stdout in completions

--- config/fish/completions/run.fish
+++ config/fish/completions/run.fish
@@ -1,3 +1,3 @@
-set -l commands (run)
+set -l commands (run 2>/dev/null)
 complete -c run -s h -l help -fx
-complete -c run -n "not __fish_seen_subcommand_from $commands" -fa "(__COMPLETE_RUN_DESCRIPTIONS=1 run)"
+complete -c run -n "not __fish_seen_subcommand_from $commands" -fa "(__COMPLETE_RUN_DESCRIPTIONS=1 run 2>/dev/null)"

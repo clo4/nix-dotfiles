296bc096ba9a07eb3c82e0a3a450f8cf54a7bd80
clo4 <robert@clo4.net>
2025-03-24 11:31:53 +1100

    fish: warn about config symlinked to store

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,7 +1,6 @@
 function fish_greeting
     if test -n "$FISH_GREETING_CHECK_SUDO_TOUCHID"
         and not grep -qE '^auth\\s+sufficient\\s+pam_tid\\.so' /etc/pam.d/sudo_local
-        echo
         gum style \
             --foreground 3 \
             --border-foreground 1 \
@@ -12,6 +11,21 @@ function fish_greeting
             --margin "1 3" \
             --padding "1 4" \
             'Touch ID will not work with sudo until the system configuration has been reapplied.'
+        echo
+    end
+
+    if string match -q "/nix/store/*" "$NIX_CONFIG_DIR"
+        gum style \
+            --foreground 3 \
+            --border-foreground 1 \
+            --bold \
+            --border rounded \
+            --align center \
+            --width 50 \
+            --margin "1 3" \
+            --padding "1 4" \
+            'Configuration symlinked to /nix/store.
+You should set a config directory.'
         echo
     end
 end

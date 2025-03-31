0e375782f9d083ba8d3c2380086af348521e3f48
clo4 <robert@clo4.net>
2025-03-31 22:34:30 +1100

    fish: use custom colors

--- config/fish/conf.d/options.fish
+++ config/fish/conf.d/options.fish
@@ -1,2 +1,28 @@
-# Exporting this allows it to be picked up by fish_indent
-set -gx fish_color_option blue
+# All of these options need to be exported for fish_indent to colorize correctly.
+
+set -gx fish_color_normal normal
+set -gx fish_color_command brcyan
+set -gx fish_color_keyword magenta
+set -gx fish_color_quote normal
+set -gx fish_color_redirection yellow --bold
+set -gx fish_color_end white
+set -gx fish_color_error red
+set -gx fish_color_param normal
+set -gx fish_color_valid_path cyan
+set -gx fish_color_option brblue
+set -gx fish_color_comment brblack
+set -gx fish_color_selection --background=brblack
+set -gx fish_color_operator yellow
+set -gx fish_color_escape cyan
+set -gx fish_color_autosuggestion brblack
+
+set -gx fish_color_cwd blue
+set -gx fish_color_cwd_root red
+set -gx fish_color_user green
+set -gx fish_color_host blue
+set -gx fish_color_host_remote yellow
+set -gx fish_color_status red
+set -gx fish_color_cancel red
+
+set -gx fish_color_search_match --background=brblack
+set -gx fish_color_history_current blue

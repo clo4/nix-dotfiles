18702641d43737e826308a36a1b06dfca76da0a1
clo4 <robert@clo4.net>
2025-07-04 17:33:17 +1000

    work-macbookpro: add home environment packages

--- hosts/work-macbookpro/users/robert/home-configuration.nix
+++ hosts/work-macbookpro/users/robert/home-configuration.nix
@@ -1,6 +1,7 @@
 {
   flake,
   config,
+  pkgs,
   ...
 }:
 {
@@ -11,8 +12,36 @@
   my.config.directory = "${config.home.homeDirectory}/Developer/clo4/nix-dotfiles";
 
   home.sessionVariables = {
-    # My fish configuration uses this to check whether it should check if
-    # the Touch ID PAM module is enabled. See: config/fish/functions/fish_greeting.fish
     FISH_GREETING_CHECK_SUDO_TOUCHID = "1";
+  };
+
+  # The work machine is managed much more imperatively than any of my personal
+  # machines, mainly because it isn't worth my time while I'm working to set
+  # up Nix correctly (without buy-in from anyone else on the team), and it
+  # isn't worth my time when I'm not working because there are other things I'd
+  # rather be doing. Instead, I can do as much as is reasonable in Nix, but
+  # fall back to plugins and homebrew when stuff doesn't work right.
+
+  home.packages = [
+    pkgs.awscli2
+    pkgs.pm2
+    pkgs.mkcert
+    pkgs.python3
+    pkgs.serverless
+
+    pkgs.ngrok
+  ];
+
+  my.programs.fish.plugins = [
+    (pkgs.fetchFromGitHub {
+      owner = "jorgebucaran";
+      repo = "nvm.fish";
+      rev = "846f1f20b2d1d0a99e344f250493c41a450f9448"; # as of 2025-07-04
+      hash = "sha256-u3qhoYBDZ0zBHbD+arDxLMM8XoLQlNI+S84wnM3nDzg=";
+    })
+  ];
+
+  home.sessionVariables = {
+    nvm_arch = "x64";
   };
 }

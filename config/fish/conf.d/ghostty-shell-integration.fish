e8c99b1e183641f4928ab8c2f76121fed82c1fc7
clo4 <robert@clo4.net>
2025-10-12 17:40:55 +1100

    fish: don't load ghostty integration if directory not present

--- config/fish/conf.d/ghostty-shell-integration.fish
+++ config/fish/conf.d/ghostty-shell-integration.fish
@@ -1,4 +1,7 @@
+# If using distrobox, it's quite possible that the variables could be set
+# but inaccessible.
 if set -q GHOSTTY_RESOURCES_DIR GHOSTTY_BIN_DIR
+    and test -d $GHOSTTY_RESOURCES_DIR -a -d $GHOSTTY_BIN_DIR
     source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
     set --append fish_complete_path "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_completions.d"
     set --append PATH $GHOSTTY_BIN_DIR

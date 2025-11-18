b7881b07145e2e726c8820e81e88ab8e4298f6c4
clo4 <robert@clo4.net>
2025-11-18 18:18:57 +1100

    work@pc3: add nvm

--- hosts/pc3/users/work/home-configuration.nix
+++ hosts/pc3/users/work/home-configuration.nix
@@ -23,12 +23,22 @@
     path = "$HOME/Repos/Work/.gitconfig";
   };
 
+  my.programs.fish.plugins = [
+    (pkgs.fetchFromGitHub {
+      owner = "jorgebucaran";
+      repo = "nvm.fish";
+      rev = "846f1f20b2d1d0a99e344f250493c41a450f9448"; # current as of 2025-11-18
+      hash = "sha256-u3qhoYBDZ0zBHbD+arDxLMM8XoLQlNI+S84wnM3nDzg=";
+    })
+  ];
+
   home.packages = [
     pkgs.awscli2
     pkgs.python3
     pkgs.vtsls
     pkgs.typos
     pkgs.typos-lsp
+    pkgs.mkcert
 
     # Useful for quick scripts and the REPL
     pkgs.deno

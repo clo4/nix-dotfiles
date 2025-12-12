3d2732f176440edbeba42cc09b47c04354c5d843
clo4 <robert@clo4.net>
2025-12-13 07:38:15 +1100

    agenix: better organize secrets file

--- secrets.nix
+++ secrets.nix
@@ -1,14 +1,23 @@
 let
   system-homeserver1 = builtins.readFile ./hosts/homeserver1/id_ed25519.pub;
+
   robert-macmini = builtins.readFile ./hosts/macmini/users/robert/id_ed25519.pub;
   robert-macbook-air = builtins.readFile ./hosts/macbook-air/users/robert/id_ed25519.pub;
-  robert-work-macbookpro = builtins.readFile ./hosts/work-macbookpro/users/robert/id_ed25519.pub;
-  # robert-pc3 = builtins.readFile ./hosts/pc3/users/robert/id_ed25519.pub;
-  work-pc3 = builtins.readFile ./hosts/pc3/users/work/id_ed25519.pub;
-  robert = [
+  robert-pc3 = builtins.readFile ./hosts/pc3/users/robert/id_ed25519.pub;
+  robert-personal = [
+    robert-pc3
     robert-macmini
     robert-macbook-air
   ];
+
+  robert-work-macbookpro = builtins.readFile ./hosts/work-macbookpro/users/robert/id_ed25519.pub;
+  work-pc3 = builtins.readFile ./hosts/pc3/users/work/id_ed25519.pub;
+  robert-work = [
+    robert-work-macbookpro
+    work-pc3
+  ];
+
+  robert = robert-personal ++ robert-work;
 in
 {
   "hosts/homeserver1/tailscale-homeserver1.age".publicKeys = [ system-homeserver1 ] ++ robert;
@@ -17,5 +26,5 @@ in
     robert-work-macbookpro
   ]
   ++ robert;
-  "hosts/pc3/users/work/gitconfig.age".publicKeys = [ work-pc3 ] ++ robert;
+  "hosts/pc3/users/work/gitconfig.age".publicKeys = robert;
 }

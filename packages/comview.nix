71e5cc2c45eb57d609ec247623adf74cfce6c55e
clo4 <robert@clo4.net>
2026-07-06 09:17:34 +1000

    comview: update to 2026-06-02

--- packages/comview.nix
+++ packages/comview.nix
@@ -1,14 +1,14 @@
 { pname, pkgs, ... }:
 pkgs.buildGoModule {
   inherit pname;
-  version = "0-unstable-2026-05-19";
+  version = "0-unstable-2026-06-02";
   src = pkgs.fetchFromGitHub {
     owner = "rockorager";
     repo = "comview";
-    rev = "7aaedd7ecdb1916be1135e4c321b1ca5e26712dd";
-    hash = "sha256-LlZ2zOVvLEvvHp+UmzrpP3nOxOpICWGKlzaGLiY7+rA=";
+    rev = "08a35f43a467783015ff12557ac02c0e30004e2f";
+    hash = "sha256-4zu6PKXRcf1j0Bavql/i/apyJt4ArhnMgW2jqAoBazs=";
   };
-  vendorHash = "sha256-K3mCrhC97/faCPAsuiexwd663H6xMdEWR7DZiafYWAA=";
+  vendorHash = "sha256-f7Q4+Bd22xnxkOWjgv4TzPmgZTNHhYvMtoVyl9anGzc=";
 
   meta = {
     description = "The best diff viewer ever made";
